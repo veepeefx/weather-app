acae6e0be84814d3b39f2a21beda8a79f8488c8c
Veepeefx <194716914+veepeefx@users.noreply.github.com>
2025-11-16 19:13:00 +0200

    controller now updates charts with real data

--- src/Controller.cpp
+++ src/Controller.cpp
@@ -1,5 +1,7 @@
 #include "Controller.h"
 
+#include <iostream>
+
 #include "mainwindow.h"
 
 #include <QObject>
@@ -26,19 +28,57 @@ void Controller::makeSearch()
         return;
     }
 
-    njson data = openMeteo_.getWeatherData(search);
+    data_ = openMeteo_.getWeatherData(search);
 
     // don't make search if search term was not valid
-    if (data.empty()) {
+    if (data_.empty()) {
         mainWindow_.getCityLabel()->setText("[Search not found!]");
         return;
     }
 
+    saveData();
+    updateUI();
+}
+
+void Controller::saveData()
+{
+    tempHourly_.clear();
+    rainHourly_.clear();
+    maxTempDaily_.clear();
+    minTempDaily_.clear();
+    rainDaily_.clear();
+
+    njson hourly = data_.at("hourly");
+    njson daily = data_.at("daily");
+
+    njson temp = hourly.at("temperature_2m");
+    njson rain = hourly.at("precipitation");
+
+    for (int i = 0; i < temp.size(); i++) {
+        tempHourly_.push_back(temp.at(i));
+        rainHourly_.push_back(rain.at(i));
+    }
+
+    njson tempMax = daily.at("temperature_2m_max");
+    njson tempMin = daily.at("temperature_2m_min");
+    rain = daily.at("precipitation_sum");
+
+    for (int i = 0; i < tempMax.size(); i++) {
+        maxTempDaily_.push_back(tempMax.at(i));
+        minTempDaily_.push_back(tempMin.at(i));
+        rainDaily_.push_back(rain.at(i));
+    }
+}
+
+void Controller::updateUI()
+{
     // adding resulted city to history and clearing searchBox
-    QString resCity = QString::fromStdString(data.at("city"));
+    QString resCity = QString::fromStdString(data_.at("city"));
     mainWindow_.getSearchBox()->addItem(resCity);
     mainWindow_.getSearchBox()->lineEdit()->setText("");
 
     // updating view
     mainWindow_.getCityLabel()->setText(resCity);
+    mainWindow_.update24hChart(tempHourly_, rainHourly_);
+    mainWindow_.update7dChart(tempHourly_, rainDaily_);
 }
--- src/Controller.h
+++ src/Controller.h
@@ -14,6 +14,22 @@ private:
     MainWindow &mainWindow_;
     OpenMeteoAPI &openMeteo_;
 
+    njson data_;
+
+    // ALL DATA STARTS FROM 0:00 DAY OF THE REQUEST AND 168h FORWARD!
+
+    // data every 1 hour for 7d (24 x 7 = 168 index)
+    std::vector<float> tempHourly_;
+    std::vector<float> rainHourly_;
+
+    // data every 24 hour for 7d (7 index)
+    std::vector<float> maxTempDaily_;
+    std::vector<float> minTempDaily_;
+    std::vector<float> rainDaily_;
+
+    void saveData();
+    void updateUI();
+
 private slots:
     void makeSearch();
 };
